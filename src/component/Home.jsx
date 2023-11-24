8777627da525eac8c0b0ba0b2dc47f257f680944
Chrismiweb <chrismibiteso@gmail.com>
2023-11-24 03:56:18 -0800

    worked on the home page

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,32 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
 import {FaLinkedin} from 'react-icons/fa';
 import {AiFillGithub} from 'react-icons/ai'
 
 function Home() {
+  const [text, setText] = useState("");
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const words = [
+    "Chrismi Bitero.",
+    "A Front-End Developer."
+  ];
+
+  const handleText = () => {
+    const currentWord = words[currentWordIndex];
+    if (text.length < currentWord.length) {
+      setText(currentWord.substring(0, text.length + 1));
+    } else {
+      setTimeout(() => {
+        setCurrentWordIndex((prevIndex) => (prevIndex === words.length - 1 ? 0 : prevIndex + 1));
+        setText("");
+      }, 1000); // Pause for 1 second before typing the next sentence
+    }
+  };
+
+  useEffect(() => {
+    const textInterval = setInterval(handleText, 100);
+    return () => {
+      clearInterval(textInterval);
+    };
+  }, [text, currentWordIndex]);
+
+
+  
   return (
     <div className='section1'>
         <div className='content-header'>
-            <h1 className='my-name'>
-              <span>I'M </span>
-              <div className='message'>
-                  <div className='word1'>CHRISMI BITERO</div>
-                  <div className='word2'>a frontend developer</div>
-              </div>
-            </h1>
+            <div className='my-name'>
+              <h1>I Am</h1>
+              <h2>{text}</h2>
+            </div>
 
-
-            <p> Welcome to my Frontend Development Showcase! 
-Step into a world where pixels come to life, and user experiences are crafted with passion. I invite you to explore my portfolio, where each click unveils a story of code, creativity, and innovation. Dive into my projects to witness the synergy of design and functionality, discover the tech behind the scenes, and understand the meticulous process that goes into each creation.
-
-<p>Curious about the mind behind the code? Head over to the "About Me" section to get acquainted. Let's connect! Whether you're seeking collaboration, have a question, or just want to chat tech, the "Contact" page is just a click away. Embark on this digital journey, and let's build something amazing together!</p>
+            <p> Welcome to my Frontend Development Showcase! Explore my portfolio to witness the synergy of design and functionality. Each click unveils a story of code, creativity, and innovation, showcasing the meticulous process behind each creation.
             </p>
+            <p>Discover the coder in the 'About Me' section. Connect on the 'Contact' page for collaboration, questions, tech chat or Job offer. Let's create something amazing together on this digital journey!</p>
+
             <Link to={'/AboutPage'}><h4>See More About Me -> </h4></Link>
+
             <div className='social-logo'>
-              <a href=""><i><FaLinkedin/></i></a>
-              <a href=""><i><AiFillGithub/></i></a>
-              <a href=""><i><FaLinkedin/></i></a>
+                <a href=""><i><FaLinkedin/></i></a>
+                <a href=""><i><AiFillGithub/></i></a>
+                <a href=""><i><FaLinkedin/></i></a>
             </div>
                 
         </div>
